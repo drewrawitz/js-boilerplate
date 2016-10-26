a8a4702cfe53d7e79da410acbe776fa6d0a8376b
Drew Rawitz <email@drewrawitz.com>
2016-10-26 03:01:06 -0400

    getting eslint to work

---
+++ src/index.js
@@ -0,0 +1,18 @@
+class Car {
+  constructor(year, make, model) {
+    Object.assign(this, { year, make, model });
+  }
+
+  static sayHi() {
+    console.log('Hey there!');
+  }
+
+  displayInfo() {
+    const result = `Nice ${this.year} ${this.make} ${this.model}!`;
+    console.log(result);
+  }
+}
+
+const camry = new Car('2012', 'Toyota', 'Camry');
+camry.sayHi();
+camry.displayInfo();
