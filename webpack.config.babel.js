a7ce8d5e6639408ee4b2d65f6713c68749be5c63
Drew Rawitz <email@drewrawitz.com>
2016-11-10 20:59:20 -0500

    adding a babel loader for our JS files

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -9,7 +9,17 @@ module.exports = env => {
       path: resolve('dist'),
       filename: 'bundle.js',
       publicPath: '/dist/',
+			pathinfo: env.prod ? false : true,
     },
     devtool: env.prod ? 'source-map' : 'eval',
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          loaders: ['babel'],
+          exclude: /node_modules/
+        },
+      ]
+    }
   })
 }
