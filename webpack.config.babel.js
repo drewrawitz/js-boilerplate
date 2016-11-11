da6ba4be5676076ce104930fd086083e8bcf7eae
Drew Rawitz <email@drewrawitz.com>
2016-11-10 20:43:29 -0500

    adding rimraf and a prebuild script

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,13 +1,15 @@
 const {resolve} = require('path')
 const webpackValidator = require('webpack-validator')
 
-module.exports = () => {
+module.exports = env => {
   return webpackValidator({
     context: resolve('src'),
     entry: './index.js',
     output: {
       path: resolve('dist'),
       filename: 'bundle.js',
+      publicPath: '/dist/',
     },
+    devtool: env.prod ? 'source-map' : 'eval',
   })
 }
